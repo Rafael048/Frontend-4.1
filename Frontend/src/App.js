bccb62f9a54e4bca0a01f0dd797804c45dafe40d
Rafael <rafael2005perezmarin@gmail.com>
2024-07-10 18:20:31 -0400

    Agregado componente de React para las cajas de texto

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import Mapview from './Components/Mapview';
 import Button from './Components/Button';
 import FormRegister from './Components/FormRegister';
 import FormLogin from './Components/FormLogin';
+import BoxText from './Components/BoxText';
 function App() {
   
   return (
@@ -15,6 +16,16 @@ function App() {
     
     <FormRegister/>
     <FormLogin/>
+
+  <BoxText
+    title = "Welcome to the world of maps"
+    text = {["José Gregorio, como le conocen cariñosamente, destacó por sus aportaciones al desarrollo de la medicina moderna en Venezuela, la generosidad con la que atendió a pacientes de bajos recursos y su fe religiosa."
+      ,"Sobresalió como introductor de la medicina científica, basada en la investigación, como profesor de varias materias novedosas en la carrera de medicina de la Universidad Central de Venezuela.",
+      "El nombre de José Gregorio Hernández fue puesto a la orden de la Iglesia católica con el calificativo del “médico de los pobres”, no obstante su don de empatía y su modo de vivir en valores hicieron que trascendiera las barreras de las clases sociales para convertirse en el 'médico de todos'.",
+      "El 19 de junio de 2020, el papa Francisco aprobó el decreto promulgado por la Congregación para la Causa de los Santos, que concede la beatificacion del doctor José Gregorio Hernández."
+    ]}
+    class = "Bg-Gray"
+  />
    </div>
     </>
    
---
+++ Frontend/src/Components/BoxText.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function BoxText(props){
+    let className = props.class
+    let title = props.title
+    let text = props.text
+
+return(
+    <section className={className}>
+        {title!=null? <h1>{title}</h1>:null}
+        {text.map((text,index)=>{
+            return(
+                <p key={index}>{text} </p>
+            )
+        })}
+
+    </section>
+)
+}
+
+export default BoxText
