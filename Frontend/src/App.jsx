e3f6273714997fe19ef2f41e2c60dc8f6ecf7bfb
RicardoBRJ-Dev <ricardobriceno1134@gmail.com>
2024-07-11 17:48:56 -0400

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,36 @@
 import './App.css';
-import Mapview from './Components/Mapview';
+//import Mapview from './Components/Mapview';
 import Button from './Components/Button';
 import FormRegister from './Components/FormRegister';
 import FormLogin from './Components/FormLogin';
 import BoxText from './Components/BoxText';
 import ImageBox from './Components/ImageBox';
+import Header from './Components/Header';
+import Landing from './Components/Landing';
+
 
 function App() {
-  
   return (
     <>
     <div>
-    <Mapview></Mapview>
+      <div className='welcome'>
+        <Header></Header>
+        <section className='Init'>
+          <div className='title-Init'>
+            <h1>Dr. José Gregorio Hernández</h1>
+          </div>
+        </section>
+      </div>
+    </div>
+    <div>
+      <Landing></Landing>
+    </div>
+    {
+      // <Mapview></Mapview>
+    }
     <Button url ="http://localhost:8000/logout" name = "LogOut"/>
-   </div>
-   <div>
+
+  <div>
     
     <FormRegister/>
     <FormLogin/>
