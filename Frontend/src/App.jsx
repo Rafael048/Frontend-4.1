fb0a47e86a903edb9da94b5c9f7ba66c997f3cd9
RicardoBRJ-Dev <ricardobriceno1134@gmail.com>
2024-07-12 10:35:16 -0400

    ajustando

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,5 @@
 import './App.css';
 //import Mapview from './Components/Mapview';
-import Button from './Components/Button';
-import FormRegister from './Components/FormRegister';
-import FormLogin from './Components/FormLogin';
-import BoxText from './Components/BoxText';
-import ImageBox from './Components/ImageBox';
 import Header from './Components/Header';
 import Landing from './Components/Landing';
 
