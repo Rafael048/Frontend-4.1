1b522d94e957e62461c4f13bc77e7b9e51704550
BerriosUmbriaJD <147112965+BerriosUmbriaJD@users.noreply.github.com>
2024-07-12 10:16:39 -0400

    Agregado el sistema de rutas de React

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -28,27 +28,7 @@ function App() {
     {
       // <Mapview></Mapview>
     }
-    <Button url ="http://localhost:8000/logout" name = "LogOut"/>
-
-  <div>
-    
-    <FormRegister/>
-    <FormLogin/>
-
-  <BoxText
-    title = "Welcome to the world of maps"
-    text = {["José Gregorio, como le conocen cariñosamente, destacó por sus aportaciones al desarrollo de la medicina moderna en Venezuela, la generosidad con la que atendió a pacientes de bajos recursos y su fe religiosa."
-      ,"Sobresalió como introductor de la medicina científica, basada en la investigación, como profesor de varias materias novedosas en la carrera de medicina de la Universidad Central de Venezuela.",
-      "El nombre de José Gregorio Hernández fue puesto a la orden de la Iglesia católica con el calificativo del “médico de los pobres”, no obstante su don de empatía y su modo de vivir en valores hicieron que trascendiera las barreras de las clases sociales para convertirse en el 'médico de todos'.",
-      "El 19 de junio de 2020, el papa Francisco aprobó el decreto promulgado por la Congregación para la Causa de los Santos, que concede la beatificacion del doctor José Gregorio Hernández."
-    ]}
-    class = "Bg-Gray"
-  />
-  <ImageBox
-    url = "/logo192.png"
-    alt = "Logo React"
-  />
-   </div>
+  
     </>
    
   );
--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FormLogin from './Components/FormLogin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<App/>} />
+        <Route path="/FormLogin" element={<FormLogin/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
