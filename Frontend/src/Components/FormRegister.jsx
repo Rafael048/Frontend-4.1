efb97bf42a4b2728bb0f2ea610fefa83b4d39390
Rafael <rafael2005perezmarin@gmail.com>
2024-07-19 16:12:24 -0400

    Agregado diseño responsive al apartado de registro

--- Frontend/src/Components/FormRegister.jsx
+++ Frontend/src/Components/FormRegister.jsx
@@ -36,9 +36,12 @@ function FormRegister(){
     return(
         
         <div className="register">
+            <div className="svg">
             <a href="/" className="home"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
             <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
             </svg></a>
+            </div>
+            
 
             <article className="logo-register">
                 <ImageBox
@@ -47,13 +50,17 @@ function FormRegister(){
                     class='image-register'
                 ></ImageBox>
             </article>
-            <form onSubmit={(e)=>handleSubmit(e)} className="form-register">
-                <h1 className="title-register">Crea tu cuenta</h1>
-                <input type="input" placeholder="Nombre" name="name" required className="input-register"></input>
-                <input type="input" placeholder="Usuario" name="user" required className="input-register"></input>
-                <input type="password" placeholder="Contraseña" name="password" required className="input-register"></input>
-                <button type="submit" className="bt-register">Registrarse</button>
-            </form>
+            <article className="article-form">
+                <form onSubmit={(e)=>handleSubmit(e)} className="form-register">
+                    <h1 className="title-register">Crea tu cuenta</h1>
+                    <input type="input" placeholder="Nombre" name="name" required className="input-register"></input>
+                    <input type="input" placeholder="Usuario" name="user" required className="input-register"></input>
+                    <input type="password" placeholder="Contraseña" name="password" required className="input-register"></input>
+                    <button type="submit" className="bt-register">Registrarse</button>
+                </form>
+                
+            </article>
+            
         </div>
     )
 }
